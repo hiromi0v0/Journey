8412ea4300e6f52bd489f1b3735af944742356fb
hiromi0v0 <maru0v0@gmail.com>
2023-05-09 15:27:09 +0900

    5.9

---
+++ photo/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import PhotoPost
+
+class PhotoPostForm(ModelForm):
+
+    class Meta:
+        model=PhotoPost
+        fields=['country','title','comment','itinerary','attribute','days1','days2','season1','season2','image1','image2','image3','image4']
--- photo/views.py
+++ photo/views.py
@@ -3,11 +3,11 @@ from django.views.generic import TemplateView,ListView
 # from django.core.paginator import Paginator
 
 # # 写真投稿ページ系
-# from django.views.generic import CreateView
-# from django.urls import reverse_lazy
-# from .forms import PhotoPostForm
-# from django.utils.decorators import method_decorator
-# from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import PhotoPostForm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 # # ------------トップページ写真投稿
 # from .models import PhotoPost
 # # ------------詳細ページ
@@ -34,23 +34,23 @@ class IndexView(TemplateView):
     template_name='index.html'
 
 
-# @method_decorator(login_required,name='dispatch')
-# class CreatePhotoView(CreateView):
+@method_decorator(login_required,name='dispatch')
+class CreatePhotoView(CreateView):
 
-#     form_class=PhotoPostForm
-#     template_name="post_photo.html"
-#     success_url=reverse_lazy('photo:post_done')
+    form_class=PhotoPostForm
+    template_name="post_photo.html"
+    success_url=reverse_lazy('photo:post_done')
 
-#     def form_valid(self, form):
-#         postdata=form.save(commit=False)
-#         postdata.user=self.request.user
-#         postdata.save()
-#         return super().form_valid(form)
+    def form_valid(self, form):
+        postdata=form.save(commit=False)
+        postdata.user=self.request.user
+        postdata.save()
+        return super().form_valid(form)
 
 
-# @method_decorator(login_required,name='dispatch')
-# class PostSuccessView(TemplateView):
-#     template_name='post_success.html'
+@method_decorator(login_required,name='dispatch')
+class PostSuccessView(TemplateView):
+    template_name='post_success.html'
 
 # # 写真投稿一覧表示
 # class IndexView(ListView):
